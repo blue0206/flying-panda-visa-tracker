2ca104414fecb9a3da47fcd12f7dd8740ede7d3f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-29 14:30:57 +0530

    feat(dto): add schema for request query validation

--- backend/src/models/alert.dto.ts
+++ backend/src/models/alert.dto.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+// Request Body Schema.
 const AlertSchema = z.object({
   country: z.string().min(1, { error: "Country is required." }),
   city: z.string().min(1, { error: "City is required." }),
@@ -10,5 +11,16 @@ const AlertSchema = z.object({
     error: "Status must be one of: Active, Booked, Expired.",
   }),
 });
-
 export type AlertDTO = z.infer<typeof AlertSchema>;
+
+// Request Query Schema.
+const AlertQuerySchema = z.object({
+  page: z.coerce.number().int().min(1).default(1),
+  limit: z.coerce.number().int().min(1).max(50).default(10),
+  country: z.string().optional(),
+  city: z.string().optional(),
+  visaType: z.enum(["Tourist", "Business", "Student"]).optional(),
+  status: z.enum(["Active", "Booked", "Expired"]).optional(),
+});
+
+export type AlertQueryDTO = z.infer<typeof AlertQuerySchema>;
