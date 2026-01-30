c59aca0c0c004e5af972d437f172567d6c0377d2
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-30 15:35:27 +0530

    feat(ui): create index file to export UI components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+export { AlertForm } from "./AlertForm";
+export { AlertTable } from "./AlertTable";
+export { AlertFilters } from "./AlertFilters";
+export { Pagination } from "./Pagination";
+export { ConfirmModal } from "./ConfirmModal";
+export { ThemeToggle } from "./ThemeToggle";
+export { ToastProvider, useToast } from "./Toast";
