e859f872f681e151ffe4b7a30781dc2ca4d3294b
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-30 15:20:59 +0530

    feat(startup): add script to run all services

---
+++ start-services.sh
@@ -0,0 +1,11 @@
+echo "Starting Docker Compose...."
+docker-compose up -d
+
+echo "Starting Flying-Panda Visa Tracking server...."
+(cd ./backend && npm run start) &
+
+echo "Starting Flying-Panda Visa Tracking UI"
+(cd ./frontend && npm run build && npm run preview) &
+
+echo "All services starting. Press Ctrl+C to close services and exit."
+wait
